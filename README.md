ebf5327ce88d4ca12202a67283637b02d9e39f92
n.maltsev <n.maltsev@drweb.com>
2016-11-30 14:43:50 +0300

    fix OPTIONS method

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ Features:
 When you open at browser first time http://localhost:9090/?monkeyhost=10.3.2.15:2341  you will be navigated on request path from host that was writen at `monkeyhost` query option.
 Also, after request browser save cookie with destination host, so you don't need to  define destination host every time you send request.
 
+#### Attentions
+- There are preflight request with OPTIONS method which is sending before cross domain xml http request(xhr). [Read at MDN](https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#Preflighted_requests) [Read at stackoverflow](http://stackoverflow.com/questions/1256593/why-am-i-getting-an-options-request-instead-of-a-get-request#answer-13030629)
+
 Run at console
 ```
 #: node index.js
--- index.js
+++ index.js
@@ -3,23 +3,35 @@ var 	$http = require('http'),
 
 
 var 	PORT = 9090,
-//		DEFAULT_HOST = '10.3.2.15:2341',
-		DEFAULT_HOST = '10.3.2.77:2341',
+		DEFAULT_HOST = '10.3.2.15:2345',
 		COOKIE_MARK = 'monkey_proxy_host',
 		QUERY_MARK = 'monkeyhost',
 		DEBUG = true;
 
 
 $http.createServer(function(request, response){
+	DEBUG && console.log('[REQ %s] `%s` %s', request.method, request.url, +new Date());
+
+	if(request.method == 'OPTIONS'){ // it is preflight request
+   		response.writeHead(200, {  
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'PROPFIND, PROPPATCH, COPY, MOVE, DELETE, MKCOL, LOCK, UNLOCK, PUT, GETLIB, VERSION-CONTROL, CHECKIN, CHECKOUT, UNCHECKOUT, REPORT, UPDATE, CANCELUPLOAD, HEAD, OPTIONS, GET, POST',
+			'Access-Control-Allow-Headers': 'Overwrite, Destination, Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control',
+			'Access-Control-Max-Age': 600, // 600s maximum at Chrome and 86400s at Firefox
+			'Content-Type': 'application/json'
+		});
+		response.end();
+		return;
+	}
+
 	var 	hostDestination,
 			conf;
 	
-	DEBUG && console.log('[REQ] `%s` %s', request.url, new Date());
-//	DEBUG && console.dir(request);	
 	
 	if(request.url.indexOf('?') != -1){
 		var queryData = $decodes.formDecode($decodes.getQuery(request.url) || '');	
 		hostDestination = queryData[QUERY_MARK];
+
 		DEBUG && console.log('Query');
 		DEBUG && console.dir(queryData);
 	}else{
@@ -34,9 +46,9 @@ $http.createServer(function(request, response){
 	conf.method = request.method;
 	conf.path = request.url;
 	conf.headers = request.headers;
-	
-//	console.log('CONF %s', hostDestination);
-//	console.dir(conf);
+
+	console.log('CONF %s', hostDestination);
+	console.dir(conf);
 	
 	var proxy_request = $http.request(conf, function(proxy_response){
 		proxy_response.on('data', function(chunk){
@@ -45,10 +57,18 @@ $http.createServer(function(request, response){
 		proxy_response.on('end', function(){
 			response.end();
 		});
+
+		// if(conf.method == 'OPTIONS'){ // it is preflight request
+		// 	proxy_response.headers['Access-Control-Max-Age'] = '600'; // 600s maximum at Chrome and 86400s at Firefox
+		// 	// Read more about: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Max-Age
+		// 	proxy_response.headers['Access-Control-Allow-Methods'] = 'PROPFIND, PROPPATCH, COPY, MOVE, DELETE, MKCOL, LOCK, UNLOCK, PUT, GETLIB, VERSION-CONTROL, CHECKIN, CHECKOUT, UNCHECKOUT, REPORT, UPDATE, CANCELUPLOAD, HEAD, OPTIONS, GET, POST';
+		// 	proxy_response.headers['Access-Control-Allow-Headers'] = 'Overwrite, Destination, Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control'; 
+		// }
 		// Fix CORS troubles
 		proxy_response.headers['Access-Control-Allow-Origin'] = '*';
-		proxy_response.headers['Access-Control-Allow-Methods'] = 'PROPFIND, PROPPATCH, COPY, MOVE, DELETE, MKCOL, LOCK, UNLOCK, PUT, GETLIB, VERSION-CONTROL, CHECKIN, CHECKOUT, UNCHECKOUT, REPORT, UPDATE, CANCELUPLOAD, HEAD, OPTIONS, GET, POST';
-		proxy_response.headers['Access-Control-Allow-Headers'] = 'Overwrite, Destination, Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control';
+		// Maybe unused
+		// proxy_response.headers['Access-Control-Allow-Methods'] = 'PROPFIND, PROPPATCH, COPY, MOVE, DELETE, MKCOL, LOCK, UNLOCK, PUT, GETLIB, VERSION-CONTROL, CHECKIN, CHECKOUT, UNCHECKOUT, REPORT, UPDATE, CANCELUPLOAD, HEAD, OPTIONS, GET, POST';
+		// proxy_response.headers['Access-Control-Allow-Headers'] = 'Overwrite, Destination, Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control';
 		
 		if(hostDestination){
 			if(proxy_response.headers['Set-Cookie']){
@@ -56,7 +76,7 @@ $http.createServer(function(request, response){
 			}else{
 				proxy_response.headers['Set-Cookie'] = COOKIE_MARK + '=' + encodeURI(hostDestination);
 			}
-			console.log('SET COOKIE `%s`', proxy_response.headers['Set-Cookie']);
+			DEBUG && console.log('SET COOKIE `%s`', proxy_response.headers['Set-Cookie']);
 		}
 	
 		response.writeHead(proxy_response.statusCode, proxy_response.headers);
